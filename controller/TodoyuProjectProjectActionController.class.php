07e2ef9dcd8d37559fabc626d56d1ad469be70a0
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2009-10-19 15:49:37 +0000

    * Rights stuff

--- controller/TodoyuProjectProjectActionController.class.php
+++ controller/TodoyuProjectProjectActionController.class.php
@@ -24,6 +24,7 @@ class TodoyuProjectProjectActionController extends TodoyuActionController {
 	public function editAction(array $params) {
 		$idProject	= intval($params['project']);
 
+
 		TodoyuProjectPreferences::saveExpandedDetails($idProject, true);
 
 		return TodoyuProjectRenderer::renderProjectEditForm($idProject);
