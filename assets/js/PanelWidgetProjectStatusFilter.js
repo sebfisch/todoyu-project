0c91ede02c82ed61ce9823af44045f8ed0855fd9
kstenschke <kstenschke@7f067039-fbb2-4aac-a504-d491c780d6a3>
2010-02-23 17:10:16 +0000

    * fixed minor JS bug (forgot several panelwidget 'inform' => 'fire' method renames, setting widgets out of inter-function)

--- assets/js/PanelWidgetProjectStatusFilter.js
+++ assets/js/PanelWidgetProjectStatusFilter.js
@@ -1,3 +1,87 @@
+<<<<<<< .mine
+/***************************************************************
+*  Copyright notice
+*
+*  (c) 2009 snowflake productions gmbh
+*  All rights reserved
+*
+*  This script is part of the todoyu project.
+*  The todoyu project is free software; you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License, version 2,
+*  (http://www.gnu.org/licenses/old-licenses/gpl-2.0.html) as published by
+*  the Free Software Foundation;
+*
+*  This script is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+*  GNU General Public License for more details.
+*
+*  This copyright notice MUST APPEAR in all copies of the script!
+***************************************************************/
+
+/**
+ *	Panel widget: StatusFilter JS
+ *
+ */
+Todoyu.Ext.project.PanelWidget.ProjectStatusFilter = {
+	
+	/**
+	 * Reference to extension js
+	 */
+	ext:	Todoyu.Ext.project,
+	
+
+	/**
+	 * PanelWidget ID
+	 */
+	key:	'projectstatusfilter',
+
+
+
+	/**
+	 * Initialize the panelWidget: setup properties, install element observers
+	 * 
+	 * @param	Array		Selected Status IDs
+	 */
+	init: function(selectedStatusIDs) {
+		this.statusFilter = new Todoyu.Ext.project.PanelWidget.StatusFilter('panelwidget-projectstatusfilter-list', this.onSelectionChange.bind(this));
+		
+			// Inject the current filter status into the project list widget
+		this.ext.PanelWidget.ProjectList.applyFilter('status', selectedStatusIDs.join(','), false);
+	},
+
+
+	/**
+	 * If clicked on an li instead on the checkbox or the label
+	 *
+	 * @param	Event		event
+	 */
+	onSelectionChange: function(event) {		
+		this.onUpdate();
+	},
+
+
+
+	/**
+	 * Handler when PanelWidget is updated
+	 */
+	onUpdate: function() {
+		Todoyu.PanelWidget.fire(this.key, this.statusFilter.getValue());
+		this.savePreference();
+	},
+
+
+
+	/**
+	 * Save the current selected statuses as preference
+	 */
+	savePreference: function() {
+		var pref	= this.statusFilter.getSelectedStatuses().join(',');
+		var action	= 'panelwidget' + this.key;
+
+		Todoyu.Pref.save('project', action, pref);
+	}
+=======
 /***************************************************************
 *  Copyright notice
 *
@@ -80,4 +164,5 @@ Todoyu.Ext.project.PanelWidget.ProjectStatusFilter = {
 
 		Todoyu.Pref.save('project', action, pref);
 	}
+>>>>>>> .r1115
 };
