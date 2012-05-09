e265c7826bae9d61cd9ec95be4d4e04a91f29491
ferni <ferni@7f067039-fbb2-4aac-a504-d491c780d6a3>
2012-05-09 15:05:24 +0000

    * Changed: Don't display dynamic project detail element, if element is falsy * Changed: Dynamic project detail element title is now parsed as label

--- config/extinfo.php
+++ config/extinfo.php
@@ -31,7 +31,7 @@ Todoyu::$CONFIG['EXT']['project']['info'] = array(
 		'company'	=> 'snowflake productions GmbH, Zurich'
 	),
 	'state'			=> 'stable',
-	'version' 		=> '1.3.0',
+	'version' 		=> '1.4.0dev',
 	'constraints'	=> array(
 		'core'		=> '2.2.0',
 		'depends'	=> array(
